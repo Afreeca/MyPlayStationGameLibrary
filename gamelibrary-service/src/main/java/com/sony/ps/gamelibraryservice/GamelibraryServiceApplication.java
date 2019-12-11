6477238fab376ce3b8a32b9b2441b29077f142a7
Adilson Mendes <adilsonmendesramos@hotmail.com>
2019-12-11 20:44:28 +0000

    adding base backend and frontend project

---
+++ gamelibrary-service/src/main/java/com/sony/ps/gamelibraryservice/GamelibraryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sony.ps.gamelibraryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelibraryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelibraryServiceApplication.class, args);
+	}
+
+}
---
+++ gamelibrary-service/src/test/java/com/sony/ps/gamelibraryservice/GamelibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sony.ps.gamelibraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
