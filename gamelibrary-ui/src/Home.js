69bd2575696e397c73122d70942418a49d7076ff
Adilson Mendes <adilsonmendesramos@hotmail.com>
2019-12-13 00:44:03 +0000

    improving home animation

--- gamelibrary-ui/src/Home.js
+++ gamelibrary-ui/src/Home.js
@@ -1,37 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typing from 'react-typing-animation';
-import { Layout } from 'antd';
+import { Button } from 'antd';
 import image from 'img/playstation-bg.png'
-
-const { Content } = Layout;
 
 
 const Home = () => {
-  return (
 
+  const [Btn, setBtnVisible] = useState(false);
+  return (
       <div className='homeContent'  style={{ backgroundImage: `url(${image})`}}>
-        <Typing className='homeText' >
-          <Typing.Delay ms={1000} />
-          <span>Since 1994 providing light at the end of the tunel!</span>
-          <Typing.Reset count={1} delay={500} />
-          <span>More then a console</span> 
-          <Typing.Delay ms={1000} />
-          <Typing.Backspace count={7} />
-          <span>game</span> 
-          <Typing.Reset count={0} delay={500} />
-          <span>We are feelings</span>  
-          <Typing.Delay ms={1000} />
-          <Typing.Delay ms={800} />
-          <span>, thoughs</span>  
-          <Typing.Delay ms={800} />
-          <span>, emotions</span>  
-          <Typing.Reset count={0} delay={500} />
-          <span>We are memories...</span>  
-          <Typing.Reset count={1} delay={1000} />
-          <span>Get your second life...</span>  
-          <Typing.Reset count={1} delay={1000} />
-          <span>NOW!</span>  
-        </Typing>
+        <div className='homeTextWrapper'>
+          <Typing className='homeText' onFinishedTyping={() => setBtnVisible(true)}
+          >
+            <Typing.Delay ms={1000} />
+            <span>Since 1994 providing light at the end of the tunel!</span>
+            <Typing.Reset count={1} delay={500} />
+            <span>More then a console</span> 
+            <Typing.Delay ms={1000} />
+            <Typing.Backspace count={7} />
+            <span>game</span> 
+            <Typing.Reset count={0} delay={500} />
+            <span>We are feelings</span>  
+            <Typing.Delay ms={1000} />
+            <Typing.Delay ms={800} />
+            <span>, thoughts</span>  
+            <Typing.Delay ms={800} />
+            <span>, emotions</span>  
+            <Typing.Reset count={0} delay={500} />
+            <span>We are memories...</span>  
+            <Typing.Reset count={1} delay={1000} />
+            <span>Get your second life...</span>  
+            <Typing.Delay ms={800} />
+            <Typing.Speed ms={200} />
+            <span> NOW!</span> 
+            <Typing.Delay ms={800} /> 
+          </Typing>
+          <div className='btnPs'>
+            { Btn && <Button ghost type="dashed" icon="rocket" size='large' onClick={() =>  window.location.href='https://www.playstation.com/en-us/'}>
+              Official website
+            </Button>}
+          </div>
+          
+        </div>
+       
       </div>
 
   );
